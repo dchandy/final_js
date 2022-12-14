59c21021a7df66ecef19360405cf1fd1c25b41b7
dchandy <dchandyp@gmail.com>
2022-12-14 02:52:44 -0700

    First version

--- js/main.js
+++ js/main.js
@@ -7,7 +7,36 @@ const inball_div = document.querySelector("#inball_div");
 
 function rand_gen (max_number){
     return Math.floor(Math.random()*max_number);
-}
+};
+
+function animation_ball () {
+    let 
+    aux=0;
+    max=20;
+    let anim_timeout = setTimeout(clear_aux, 2000);
+    let anim_timer = setInterval(myTimer, 10);
+    function myTimer() {
+        aux += 15;
+        inball_div.style.top = Math.round(aux) + "px";
+        if (aux >= ball_div.clientHeight) {
+            aux = -150;
+        };
+    };
+    function clear_aux () {
+        clearInterval(anim_timer);
+        inball_div.style.top = "0px";
+        answer.style.fontSize = "20px";
+        answer.innerText = ans_pool[rand_gen(15)];
+        let anim_timeout2 = setTimeout(clear_aux2, 3000);
+    };
+    function clear_aux2 () {
+        question.value = "";
+        question.placeholder = "Ask me anything!!";
+        answer.innerText = "8";
+        answer.style.fontSize = "100px";
+    };
+    return;
+};
 
 const ans_pool = {
     0: "It is certain.",
@@ -29,9 +58,7 @@ const ans_pool = {
 
 button.addEventListener("click", () => {
     if (question.value.length >= 2) {
-        answer.style.fontSize = "20px";
-        answer.innerText = ans_pool[rand_gen(15)];
-        
+        animation_ball();
     }else {
         question.placeholder = "Ask me anything!!";
         answer.innerText = "8";
@@ -39,16 +66,20 @@ button.addEventListener("click", () => {
     };
 });
 
+ball_div.addEventListener("mousemove",function(e){
+    // let new_top = e.layerY - ball_div.offsetTop - (inball_div.clientHeight);
+    // inball_div.style.top = `${new_top}px`;
+    // let new_offset = e.offsetX - inball_div.clientWidth/2;
+    // inball_div.style.left = `${new_offset}px`;
+});
+
 ball_div.addEventListener("mouseenter", () => {
-    ball_div.style.backgroundColor = "green";
-    ball_div.addEventListener("mousemove",function(e){
-        console.log(`Movement X: ${e.screenX}, Y: ${e.screenY}`);
-        inball_div.style.top = `${e.screenY}`;
-    });
+
 });
 
 ball_div.addEventListener("mouseleave", () => {
-    ball_div.style.backgroundColor = "black";
+    inball_div.style.top = "0px";
+    inball_div.style.left = "0px";
 });
 
 question.addEventListener("input", () => {
