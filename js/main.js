6eb5b91654f01af4e584ca4d8c118a3e45be6cdf
dchandy <dchandyp@gmail.com>
2022-12-14 04:43:19 -0700

    Add movement to 8 ball

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@ const button = document.querySelector("#button_8");
 const answer = document.querySelector("#answer");
 const ball_div = document.querySelector("#ball_div");
 const inball_div = document.querySelector("#inball_div");
+let isDrawing = false;
+let x = 0;
+let y = 0;
 
 
 function rand_gen (max_number){
     return Math.floor(Math.random()*max_number);
 };
 
-function animation_ball () {
+function animation_ball (slope) {
     let 
     aux=0;
     max=20;
@@ -17,7 +20,8 @@ function animation_ball () {
     let anim_timer = setInterval(myTimer, 10);
     function myTimer() {
         aux += 15;
-        inball_div.style.top = Math.round(aux) + "px";
+        inball_div.style.left = aux + "px";
+        inball_div.style.top = Math.round(slope*aux) + "px";
         if (aux >= ball_div.clientHeight) {
             aux = -150;
         };
@@ -25,6 +29,7 @@ function animation_ball () {
     function clear_aux () {
         clearInterval(anim_timer);
         inball_div.style.top = "0px";
+        inball_div.style.left = "0px";
         answer.style.fontSize = "20px";
         answer.innerText = ans_pool[rand_gen(15)];
         let anim_timeout2 = setTimeout(clear_aux2, 3000);
@@ -37,6 +42,10 @@ function animation_ball () {
     };
     return;
 };
+
+function line (x,y,x1,y1) {
+    return [(y1 -y)/(x1 -x),(x1*y-x*y1)/(x1-x)];
+}
 
 const ans_pool = {
     0: "It is certain.",
@@ -58,23 +67,26 @@ const ans_pool = {
 
 button.addEventListener("click", () => {
     if (question.value.length >= 2) {
-        animation_ball();
+        animation_ball(0);
     }else {
-        question.placeholder = "Ask me anything!!";
+        question.placeholder = "I'm the seer, ask me anything!!";
         answer.innerText = "8";
         answer.style.fontSize = "100px";
     };
 });
 
-ball_div.addEventListener("mousemove",function(e){
-    // let new_top = e.layerY - ball_div.offsetTop - (inball_div.clientHeight);
-    // inball_div.style.top = `${new_top}px`;
-    // let new_offset = e.offsetX - inball_div.clientWidth/2;
-    // inball_div.style.left = `${new_offset}px`;
+ball_div.addEventListener("mousedown",function(e){
+    x = e.screenX;
+    y = e.screenY;
 });
-
-ball_div.addEventListener("mouseenter", () => {
-
+ball_div.addEventListener("mouseup",function(e){
+    if (question.value.length >= 2) {
+        animation_ball(line(x,y,e.screenX,e.screenY)[0]);
+    }else {
+        question.placeholder = "Don't be afraid, ask me anything!!";
+        answer.innerText = "8";
+        answer.style.fontSize = "100px";
+    };
 });
 
 ball_div.addEventListener("mouseleave", () => {
